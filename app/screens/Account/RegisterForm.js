98d4f2dbfaaf9a9ea489c51599a61f3d3da09d2b
Juan Manuel Ros <jmrosdev@gmail.com>
2022-01-27 21:06:48 +0100

    add login with user and mail

--- app/screens/Account/RegisterForm.js
+++ app/screens/Account/RegisterForm.js
@@ -2,20 +2,24 @@ import React, {useState} from 'react'
 import { StyleSheet, Text, View, Image } from 'react-native'
 import { Input, Icon, Button } from 'react-native-elements'
 import { COLOR_PRIMARY } from '../../constants'
-import { validateEmail, formRegisterValidate } from '../../utils/validations'
-
+import { formRegisterValidate } from '../../utils/validations'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
 
 export default function RegisterForm (props) {
     const { toastRef } = props
     const [formData, setFormData] = useState(defaultValueForm())
-    console.log('toastRef',toastRef)
+    const auth = getAuth();
+    
     const onSubmit = () => {
-        const { isValidForm, errorType } = formRegisterValidate(formData)
+        const { isValidForm, errorType } = formRegisterValidate(formData)        
         
         if (!isValidForm) {
             toastRef.current.show(errorType)
+        } else {
+            createUserWithEmailAndPassword(auth, formData.email, formData.password)
+                .then(response => console.log(response))
+                .catch(error => console.log('ERROOOOOOR',error))
         }
-
     }
 
     const onChange = (event, type) => {
