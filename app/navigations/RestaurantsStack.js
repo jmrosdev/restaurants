9acbc3b674c7ad96011a078fcc80e8f6496ec50e
Juan Manuel Ros <jmrosdev@gmail.com>
2022-02-02 21:39:22 +0100

    create button add restaurant and show when user is logged

--- app/navigations/RestaurantsStack.js
+++ app/navigations/RestaurantsStack.js
@@ -1,13 +1,17 @@
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import Restaurants from '../screens/Restaurants'
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Restaurants from "../screens/Restaurants/Restaurants";
 
 const Stack = createNativeStackNavigator();
 
 export default function RestaurantsStack() {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name="restaurants-stack" component={Restaurants} options={{title: "Restaurantes"}} />
-        </Stack.Navigator>
-    )
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="restaurants-stack"
+        component={Restaurants}
+        options={{ title: "Restaurantes" }}
+      />
+    </Stack.Navigator>
+  );
 }
---
+++ app/screens/Restaurants/Restaurants.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { Icon } from "react-native-elements";
+// import { firebaseApp } from "../../service/firebase";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+import { COLOR_PRIMARY, COLOR_WHITE } from "../../constants";
+
+export default function Restaurants() {
+  const [user, setUser] = useState(null);
+  const auth = getAuth();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (info) => {
+      // If user not logged the state is null, if user logged setUser will be an object
+      setUser(info);
+    });
+  }, []);
+
+  return (
+    <View style={styles.viewBody}>
+      <Text>Restaurantes</Text>
+      {user && (
+        <Icon
+          type="material-community"
+          name="plus"
+          color={COLOR_PRIMARY}
+          reverse
+          containerStyle={styles.btnContainer}
+        />
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  viewBody: {
+    flex: 1,
+    backgroundColor: COLOR_WHITE,
+  },
+  btnContainer: {
+    position: "absolute",
+    bottom: 10,
+    right: 10,
+    shadowColor: "black",
+    shadowOffset: {
+      width: 10,
+      height: 10,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+  },
+});
